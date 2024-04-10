417bf02c8a15c3d4d6ebad28a2068d56b16a6a9e
MAGMA <instafollacc@gmail.com>
2024-04-10 09:01:34 -0400

    Improved

--- Algorithms/LinearSearch.java
+++ Algorithms/LinearSearch.java
@@ -1,34 +1,74 @@
 // jeninsutradhar@gmail.com
-// created: 09 April 2024
-// Simple Linear Search Algorithm
+// https://github.com/JeninSutradhar/JAVA-DataStructures-Algorithms
 
+// Linear Search Algorithm
+/*
+ * Linear Search: Iterate through a Collection One Element at a time
+ *                  
+ *                Runtime Complexity = O(n)
+ *              - Fast for searches of small-medium Data Sets
+ *              - Does not need to be Sorted
+ *              - Useful for linked Lists
+ */
 public class LinearSearch {
 
-    // Method to perform linear search
-    public static int linearSearch(int[] array, int target) {
-        // Traverse through each element of the array
+    public static void main(String[] args) {
+
+        // Integer array
+        int[] intArray = { 1, 5, 7, 4, 7, 9, 32, 6, 12, 7, 9, 0, 23, 2 };
+        int intValue = 9;
+        int intIndex = linearSearch(intArray, intValue);
+
+        // Print results for integer search
+        if (intIndex != -1) {
+            System.out.println("Integer Element Found At Index: " + intIndex);
+        } else {
+            System.out.println("Integer Search Failed! [Error: Element not Found]");
+        }
+
+
+        // String array
+        String[] stringArray = { "Java", "Python", "C++", "JavaScript", "Ruby", "C", "Rust", "Typescript" };
+        String stringValue = "Rust";
+        int stringIndex = linearSearch(stringArray, stringValue);
+
+        // Print results for string search
+        if (stringIndex != -1) {
+            System.out.println("String Element Found At Index: " + stringIndex);
+        } else {
+            System.out.println("String Search Failed! [Error: Element not Found]");
+        }
+    }
+
+    /**
+     * Performs a linear search on an array of integers.
+     *
+     * @param array The array to search.
+     * @param value The value to search for.
+     * @return The index of the value if found, -1 otherwise.
+     */
+    private static int linearSearch(int[] array, int value) {
         for (int i = 0; i < array.length; i++) {
-            // If the current element equals the target, return its index
-            if (array[i] == target) {
+            if (array[i] == value) {
                 return i;
             }
         }
-        // If the target is not found, return -1
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] numbers = { 10, 20, 30, 40, 50, 60 };
-        int target = 30;
-
-        // Perform linear search
-        int index = linearSearch(numbers, target);
-
-        // Check if the target is found
-        if (index != -1) {
-            System.out.println("Element found at index: " + index);
-        } else {
-            System.out.println("Element not found");
+    /**
+     * Performs a linear search on an array of strings.
+     *
+     * @param array The array to search.
+     * @param value The value to search for.
+     * @return The index of the value if found, -1 otherwise.
+     */
+    private static int linearSearch(String[] array, String value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(value)) {
+                return i;
+            }
         }
+        return -1;
     }
 }
